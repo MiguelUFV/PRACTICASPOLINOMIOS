66a16cd4a28098aeb943865301f1ff7e0d752c93
jaimeantonufv <9301203@alumnos.ufv.es>
2025-03-11 09:32:38 +0100

    he hecho el bucle

--- funciones/calculadora.py
+++ funciones/calculadora.py
@@ -2,6 +2,20 @@
 def parse_polinomio(polinomio_str):
     terminos = polinomio_str.replace('-', '+-').split('+')
     polinomio = {}
+    for termino in terminos:
+        termino = termino.strip()
+        if 'x^' in termino:
+            coef, exp = termino.split('x^')
+            exp = int(exp)
+        elif 'x' in termino:
+            coef = termino.replace('x', '')
+            exp = 1
+        else:
+            coef = termino
+            exp = 0
+        coef = float(coef) if coef else 1.0
+        polinomio[exp] = polinomio.get(exp, 0) + coef
+    return polinomio
 
 
 
