b913ca243c323e4c44da4fa2f815a40e98e5c864
MiguelUFV <9302286@alumnos.ufv.es>
2025-03-14 09:27:16 +0100

    Opcion 4

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ def main():
 
         if opcion == '5':
             break
+   if opcion == '4':
+            p = parse_polinomio(input("Escribe el polinomio a evaluar (ej: 3x^2+3x+1): "))
+            x = float(input("Introduce el valor de x: "))
+            print("Resultado de la evaluación:", evalua(p, x))
+            continue
+
+        p1 = parse_polinomio(input("Escribe el primer polinomio (ej: 3x^2+3x+1): "))
+        p2 = parse_polinomio(input("Escribe el segundo polinomio (ej: x^2-1): "))
+
