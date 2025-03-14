933d58eaaae9b218b9dc8f3100f6e001e50bec9f
jaimeantonufv <9301203@alumnos.ufv.es>
2025-03-14 09:29:26 +0100

    he hecho las ultimas opciones y que hace cuando se ejecuta

--- main.py
+++ main.py
@@ -22,3 +22,18 @@ def main():
         p1 = parse_polinomio(input("Escribe el primer polinomio (ej: 3x^2+3x+1): "))
         p2 = parse_polinomio(input("Escribe el segundo polinomio (ej: x^2-1): "))
 
+   if opcion == '1':
+            resultado = suma(p1, p2)
+        elif opcion == '2':
+            resultado = resta(p1, p2)
+        elif opcion == '3':
+            resultado = multiplica(p1, p2)
+        else:
+            print("Opción no válida")
+            continue
+
+        print("Resultado:", imprime_polinomio(resultado))
+
+if __name__ == "__main__":
+    main()
+
