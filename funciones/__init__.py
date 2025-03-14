8d013a1092619002c7c4ac9c1523d93a61e908cd
MiguelUFV <9302286@alumnos.ufv.es>
2025-03-14 09:18:38 +0100

    Definido los distintos prints en pantalla

--- funciones/__init__.py
+++ funciones/__init__.py
@@ -1,1 +1,13 @@
 from funciones.calculadora import parse_polinomio, suma, resta, multiplica, imprime_polinomio, evalua
+def main():
+    while True:
+        print("\nCalculadora de Polinomios")
+        print("1) Sumar")
+        print("2) Restar")
+        print("3) Multiplicar")
+        print("4) Evaluar")
+        print("5) Salir")
+        opcion = input("Elige una opción: ")
+
+        if opcion == '5':
+            break
